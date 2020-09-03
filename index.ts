59e6b7034eb14d086f571f30672add1012a062ac
gutamaarlian <masihadabesok@gmail.com>
2020-09-03 09:09:22 +0700

    first commit

---
+++ index.ts
@@ -0,0 +1,28 @@
+let num1 : number;
+let num2 : number;
+let res : number;
+
+num1 = 10;
+num2 = 2;
+res = 0;
+
+res = num1 + num2;
+console.log("Sum            : " + res);
+
+res = num1 - num2;
+console.log("Differrence    : " + res);
+
+res = num1 * num2;
+console.log("Product        : " + res);
+
+res = num1 / num2;
+console.log("Quotient       : " + res);
+
+res = num1 % num2;
+console.log("Remainder      : " + res);
+
+num1++;
+console.log("Value of num1 after increment : " + num1);
+
+num2--;
+console.log("Value of num2 after increment : " + num2);
